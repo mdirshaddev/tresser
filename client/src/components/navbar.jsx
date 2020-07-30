6acbb966b7a259caaf5eb7809256329b5d575442
mdirshaddev <mdirshaddev@gmail.com>
2020-07-30 12:13:09 +0530

    Mobile responsive header with a breadcrum.

--- client/src/components/navbar.jsx
+++ client/src/components/navbar.jsx
@@ -1,27 +1,46 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React, { Component } from 'react';
+ import { Link } from 'react-router-dom';
 
-export default class NavBar extends React.Component{
-    render(){
-        return(
-            <div className="container">
-              <nav className="navbar navbar-dark bg-primary navbar-expand-lg">
-                  <Link to="/" className="navbar-brand">Exercise Tracker</Link>
-                  <div className="collapse navbar-collapse">
-                      <ul className="navbar-nav mr-auto">
-                      <li className="navbar-item">
-                          <Link to="/" className="nav-link">Exercises</Link>
-                      </li>
-                      <li className="navbar-item">
-                          <Link to="/create" className="nav-link">Create Exercise log</Link>
-                      </li>
-                      <li className="navbar-item">
-                          <Link to="/user" className="nav-link">Create User</Link>
-                      </li>
-                      </ul>
-                  </div>
-              </nav>
-            </div>
-        )
-    }
-}
+class Nav extends Component {
+  constructor(props) {
+    super(props);
+    this.toggleNavbar = this.toggleNavbar.bind(this);
+    this.state = {
+      collapsed: true,
+    };
+  }
+  toggleNavbar() {
+    this.setState({
+      collapsed: !this.state.collapsed,
+    });
+  }
+  render() {
+    const collapsed = this.state.collapsed;
+    const classOne = collapsed ? 'collapse navbar-collapse' : 'collapse navbar-collapse show';
+    const classTwo = collapsed ? 'navbar-toggler navbar-toggler-right collapsed' : 'navbar-toggler navbar-toggler-right';
+      return (
+        <nav className="navbar navbar-expand-lg navbar-dark bg-primary transparent-nav">
+          <div className="container">
+            <a className="navbar-brand" href="#">Rate My Neighborhood</a>
+            <button onClick={this.toggleNavbar} className={`${classTwo}`} type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
+            <span className="navbar-toggler-icon" />
+            </button>
+          <div className={`${classOne}`} id="navbarResponsive">
+            <ul className="navbar-nav ml-auto">
+              <li className="nav-item active">
+                <Link to="/" className="nav-link">Exercises</Link>
+              </li>
+              <li className="nav-item">
+                <Link to="/create" className="nav-link">Create Exercise log</Link>
+              </li>
+              <li className="nav-item">
+                <Link to="/user" className="nav-link">Create User</Link>
+              </li>
+            </ul>
+          </div>
+          </div>
+        </nav>
+      );
+  }
+ }
+export default Nav;
